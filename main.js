48c4ec6dde2740e2fdbdf01641fe9408cce92f00
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-04-23 15:16:41 +0200

    [UPD] added print to browser

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ let currentElement = document.getElementById('password');
 let currentText = currentElement.innerHTML;
 
 //prompt nome-salvo in una variabile userName
-const userName = prompt('Inserisci il tuo nome')
+const userName = prompt('Inserisci il tuo nome');
 
 //prompt cognome-salvo in una variabile userLastName
-const userLastName = prompt('Inserisci il cognome')
+const userLastName = prompt('Inserisci il cognome');
 
 //prompt colore-salvo in una variabile userFavoriteColor
-const userFavoriteColor = prompt ('Inserisci colore preferito')
+const userFavoriteColor = prompt ('Inserisci colore preferito');
 
-currentText = `${currentText} ${userName} ${userLastName} ${userFavoriteColor}`
+currentText = `${currentText}${userName}${userLastName}${userFavoriteColor}`;
+
+//Scrivo nel browser
+currentElement.innerHTML = currentText;
+
+
 
