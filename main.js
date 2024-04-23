619e6e5c8f042a21817b535ff20caf82e7cc4740
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-04-23 14:55:14 +0200

    [UPD] set-up variables in js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 //mette un freno a js per costringerlo ad usare delle best practices
 'use strict';
 
+let currentElement = document.getElementById('password');
+let currentText = currentElement.innerHTML;
+
+//prompt nome-salvo in una variabile userName
+const userName = prompt('Inserisci il tuo nome')
+
+//prompt cognome-salvo in una variabile userLastName
+const userLastName = prompt('Inserisci il cognome')
+
+//prompt colore-salvo in una variabile userFavoriteColor
+const userFavoriteColor = prompt ('Inserisci colore preferito')
+
+currentText = `${currentText} ${userName} ${userLastName} ${userFavoriteColor}`
+
